a647b538b5e21b3070327a49586cb9578b133ade
Miko Dierks <stu213123@mail.uni-kiel.de>
2022-08-29 16:00:32 +0200

    Fix incorrect method call for file name on bsl address

--- node/experiment/modules/zoul.py
+++ node/experiment/modules/zoul.py
@@ -8,6 +8,7 @@ class ZoulExperimentModule(ExperimentModule):
     def __init__(self, *args, **kwargs):
         super(ZoulExperimentModule, self).__init__(*args, **kwargs)
         self.bsl_address_path = self.firmware_path.with_suffix("-bsl-address.txt")
+        self.bsl_address_path = self.firmware_path.with_name(self.firmware_path.name + "-bsl-address.txt")
 
 
     def prepare(self):
