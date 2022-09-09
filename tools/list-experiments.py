535489537da5b12c272903a95ed757c11c687ac3
Miko Dierks <stu213123@mail.uni-kiel.de>
2022-09-09 12:19:57 +0200

    Improve readability of experiment status for list-experiments

--- tools/list-experiments.py
+++ tools/list-experiments.py
@@ -11,13 +11,22 @@ import tools.request as request
 from tools.configuration import get_server_address
 
 
-class ExperimentStatus(Enum):
-    CREATED = "CREATED"
-    SCHEDULED = "SCHEDULED"
-    STARTED = "STARTED"
-    FAILED_TO_RETRIEVE_LOGS = "FAILED_TO_RETRIEVE_LOGS"
-    CANCELLED = "CANCELLED"
-    DONE = "DONE"
+class ExperimentStatus(str, Enum):
+    display_value: str
+
+    def __new__(cls, name: str, display_value: str):
+        obj = str.__new__(cls, name)
+        obj._value_ = name
+
+        obj.display_value = display_value
+        return obj
+
+    CREATED = ("CREATED", "Created")
+    SCHEDULED = ("SCHEDULED", "Scheduled")
+    STARTED = ("STARTED", "Started")
+    FAILED_TO_RETRIEVE_LOGS = ("FAILED_TO_RETRIEVE_LOGS", "Failed to retrieve all logs")
+    CANCELLED = ("CANCELLED", "Cancelled")
+    DONE = ("DONE", "Done")
 
 
 def get_experiment_list(server_address: str, start_delta: timedelta = timedelta(hours=1), end_delta: timedelta = timedelta(hours=12)):
@@ -48,6 +57,8 @@ def print_experiment_list(experiment_list: list):
     for experiment in experiment_list:
         try:
             status = ExperimentStatus(experiment["status"])
+            experiment["status"] = status
+
             if status == ExperimentStatus.CREATED:
                 list_created.append(experiment)
             elif status == ExperimentStatus.SCHEDULED:
@@ -68,7 +79,7 @@ def print_experiment_list(experiment_list: list):
 def print_list(experiment_list, is_last = False):
     for experiment in experiment_list:
         print(
-            f'{experiment["name"]} (ID {experiment["id"]})\t[{experiment["status"]}]:\t{datetime(*experiment["start"])}\t->\t{datetime(*experiment["end"])}'
+            f'{experiment["name"]} (ID {experiment["id"]})\t[{experiment["status"].display_value}]:\t{datetime(*experiment["start"])}\t->\t{datetime(*experiment["end"])}'
         )
 
     if len(experiment_list) > 0 and not is_last:
